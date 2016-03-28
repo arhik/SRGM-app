74b3632c60766ce01165bcb768854cb111065fd2
arhik <karthik.katipalli@gmail.com>
2016-03-27 22:05:10 -0400

    sample file loader function and changes to reflect the changes in server port

--- src/csv_upload.js
+++ src/csv_upload.js
@@ -34,14 +34,22 @@ export class Upload{
 		console.log("Hello i am being triggered");
 		let data = new FormData();
 		data.append('file', this.datafile[0]);
-		this.http.fetch("http://localhost:3300/upload",{method:"post",body: data}).then(response => response.json()).then(formatted_data => this.eventAggregator.publish('rawdata', JSON.parse(formatted_data)));
+		this.http.fetch("/upload",{method:"post",body: data}).then(response => response.json()).then(formatted_data => this.eventAggregator.publish('rawdata', JSON.parse(formatted_data)));
 		this.msg = "Hello hi. CSV is sucessfully sent";
 	}
 
 	computedata(){
 		console.log("Compute is triggered");
 		console.log(this.originaldata.getRawData());
-		this.http.fetch("http://localhost:3300/compute",{method: "post", body: this.originaldata.getRawData()}).then(response => response.json()).then(formattedplot_data => this.eventAggregator.publish('plotData',formattedplot_data));
+		this.http.fetch("/compute",{method: "post", body: this.originaldata.getRawData()}).then(response => response.json()).then(formattedplot_data => this.eventAggregator.publish('plotData',formattedplot_data));
 		this.msg = "Plot data is here"
+	}
+
+	load_sample(){
+		console.log("Loading sample data ...");
+		this.http.fetch("/sample",{method:"get"}).then(response => response.json()).then(formatted_data => this.eventAggregator.publish('rawdata', JSON.parse(formatted_data)));
+		this.msg = "sample is loaded for you."
+		console.log("Completed loading sample");
+
 	}
 }
